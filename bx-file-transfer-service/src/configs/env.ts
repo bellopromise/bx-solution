abd0a54d2fad3bd33bddba4000e8164e65f68c07
Bello Promise <p.bello@axelor.com>
2023-11-26 22:58:51 +0100

    #2 - Updated the env config

--- bx-file-transfer-service/src/configs/env.ts
+++ bx-file-transfer-service/src/configs/env.ts
@@ -15,6 +15,8 @@ export const ENV_NAME = process.env.ENV_NAME;
 
 export enum TopicForSubscriptionCollection {
   example = '/example/+',
+  fileUploadRequest = 'file/upload/request',
+  fileDownloadRequest = 'file/download/request'
 }
 
 export const SERVER_PORT = process.env.SERVER_PORT;
@@ -24,3 +26,8 @@ export const MQTT_HOST = process.env.MQTT_HOST;
 export const MQTT_PORT = process.env.MQTT_PORT;
 export const MQTT_PROTOCOL = process.env.MQTT_PROTOCOL;
 export const MQTT_URL = `${MQTT_PROTOCOL}://${MQTT_HOST}:${MQTT_PORT}`;
+export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+export const AWS_S3_FORCE_PATH_STYLE = process.env.AWS_S3_FORCE_PATH_STYLE;
+export const AWS_S3_ENDPOINT = process.env.AWS_S3_ENDPOINT;
