46a9f53f9db48d044733ab3c7cc06f3204c882a0
Bello Promise <p.bello@axelor.com>
2023-11-26 23:41:26 +0100

    #7 - Modified bootstrap function

--- bx-file-transfer-service/src/main.ts
+++ bx-file-transfer-service/src/main.ts
@@ -8,6 +8,7 @@ import mqttConfig from './configs/mqtt';
 import { DtoValidationException } from './exceptions/dto-validation-exception';
 import { AppModule } from './modules/app.module';
 import { handleBootstrapIssue } from './utils/handle-bootstrap-issue';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -33,12 +34,22 @@ async function bootstrap() {
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(globalValidationPipe);
   app.enableShutdownHooks();
-
+  
   process.on('SIGTERM', function beforeExitFromProcess() {
     loggerService.log('Exit signal');
     app.close();
     process.exit(0);
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('File Transfer API')
+  .setDescription('The file transfer API for upload and download')
+  .setVersion('1.0')
+  .addTag('file-transfer')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.startAllMicroservices();
   await app.listen(3000);
